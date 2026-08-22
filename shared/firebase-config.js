3d39e9e57dc80d09ec2ef0669c9b891e1751d35e
Peter Rauscher <peter@peterrauscher.com>
2026-08-22 15:11:53 -0700

    chore: add Firebase Spark project config
    Pin the block-receipts Firebase app, deploy per-user Firestore rules, and
    store the public web SDK config for later Google sign-in.

---
+++ shared/firebase-config.js
@@ -0,0 +1,11 @@
+(() => {
+  globalThis.BlockReceiptsFirebase = {
+    projectId: "block-receipts",
+    appId: "1:177311989333:web:b095a48fe9d0dd44095edc",
+    apiKey: "AIzaSyAqtd5_Y-aNs4EX3NubYJi9Okx9-SDZp2U",
+    authDomain: "block-receipts.firebaseapp.com",
+    storageBucket: "block-receipts.firebasestorage.app",
+    messagingSenderId: "177311989333",
+    googleClientId: "177311989333-etbadocucos9vjav7s3j4aqfb25o7nva.apps.googleusercontent.com"
+  };
+})();
